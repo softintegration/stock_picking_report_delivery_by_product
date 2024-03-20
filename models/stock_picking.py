2ca4b5dcf92ca4b4ed4eeb502d716f44d62c424a
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2024-03-20 11:08:51 +0100

    [IMP] Delivery report:Add «Serial/lot number» columb

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -15,12 +15,20 @@ class Picking(models.Model):
         lines_by_product = []
         for line in self.move_lines.filtered(lambda mv:mv.state != 'cancel' and mv.product_uom_qty):
             if lines_by_product_dict.get(line.product_id):
+                # lots
+                if line.move_line_ids and line.state == 'done' and line.move_line_ids.mapped('lot_id'):
+                    lines_by_product_dict[line.product_id]['serial_numbers'] |= line.move_line_ids.mapped('lot_id')
                 lines_by_product_dict[line.product_id]['product_uom_qty'] += line.product_uom_qty
                 lines_by_product_dict[line.product_id]['quantity_done'] += line.quantity_done
             else:
-                lines_by_product_dict.update({line.product_id:{'product_uom_qty':line.product_uom_qty,
-                                                          'quantity_done':line.quantity_done,
-                                                          'product_uom':line.product_uom}})
+                if self.move_line_ids and self.state == 'done' and self.move_line_ids.mapped('lot_id'):
+                    serial_numbers = self.move_line_ids.mapped('lot_id')
+                else:
+                    serial_numbers = self.env['stock.production.lot']
+                lines_by_product_dict.update({line.product_id:{'serial_numbers':serial_numbers,
+                                                                'product_uom_qty':line.product_uom_qty,
+                                                                'quantity_done':line.quantity_done,
+                                                                'product_uom':line.product_uom}})
         for product,line in lines_by_product_dict.items():
             line.update({'product_id':product})
             lines_by_product.append(line)
